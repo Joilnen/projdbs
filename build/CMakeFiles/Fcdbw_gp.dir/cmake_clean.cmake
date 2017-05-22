98b7598f24c16e0af654cb1ef04304174552b9df
Joilnen <joilnen@gmail.com>
2017-05-22 09:32:53 -0300

    initial commit

---
+++ build/CMakeFiles/Fcdbw_gp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Fcdbw_gp.dir/src/DbArango.cpp.o"
+  "CMakeFiles/Fcdbw_gp.dir/src/DbSedna.cpp.o"
+  "CMakeFiles/Fcdbw_gp.dir/src/FcdbwGp.cpp.o"
+  "bin/linux/Fcdbw_gp.pdb"
+  "bin/linux/Fcdbw_gp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Fcdbw_gp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
